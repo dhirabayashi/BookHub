363a89e200c4042f90b415539d532f5e80969527
Daiki Hirabayashi <gigabowser@hotmail.co.jp>
2016-10-19 00:20:30 +0900

    コミット漏れ

--- Gemfile
+++ Gemfile
@@ -3,3 +3,6 @@ source "https://rubygems.org"
 
 gem 'sinatra', '1.4.7'
 gem 'sinatra-contrib', '1.4.7'
+gem 'activerecord', '5.0.0.1'
+gem 'sinatra-activerecord', '2.0.11'
+gem 'sqlite3', '1.3.12'
--- main.rb
+++ main.rb
@@ -1,10 +1,36 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require './models/user.rb'
 
 get '/' do
     erb :login
 end
 
 post '/login' do
-    "id=#{request['id']}, password=#{request['password']}"
+    if User.exists?(name: request['name'], password: request['password'])
+        erb :index
+    else
+        erb :login
+    end
+end
+
+# menu
+get 'index' do
+    erb :index
+end
+
+# Users
+get '/add_user' do
+    erb :add_user
+end
+
+post '/add_user' do
+    u = User.new
+    u.name = request['name']
+    u.password = request['password']
+    u.mail = request['mail']
+    
+    u.save!
+    
+    erb :login
 end
